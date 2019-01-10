54d5e927cbdfecfd0b9e23281521fb0b2fe79468
Eyal Toledano <eyaltoledano@me.com>
2019-01-10 17:12:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 
 # 1. What is the method called on? The general scope
 # 2. What arguments does the method take? Takes a number as an argument
-# 3. What does the method do? It checks if the argument is a prime number 
+# 3. What does the method do? It checks if the argument is a prime number
 # 4. What does the return value of the method? true or false
 
 # Prime numbers can only be divided evenly (without remainder) by or 1 or itself.
@@ -10,10 +10,13 @@
 def prime?(number)
  # return false if number is not a whole number
  return false if !number.digit?
+
  # return false if number is under 2
  return false if number < 2
+
  # return false if the number can be divided evenly by any number preceding the number
  (2..number-1).each {|digit| return false if digit % number == 0}
+ 
  # returns true if the number cannot be divided evenly by any number preceding it
  true
 end
